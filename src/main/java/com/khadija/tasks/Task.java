953f41fc2b90842f51796af6b149ee37a2dc6188
Xedice Abbasova <abbasovaxdc@gmail.com>
2024-10-10 23:45:36 +0400

    Initial commit

---
+++ src/main/java/com/khadija/tasks/Task.java
@@ -0,0 +1,22 @@
+package com.khadija.tasks;
+
+import java.util.UUID;
+
+public class Task {
+
+    private final String id;
+    private final String description;
+
+    public Task(String description) {
+        this.id = UUID.randomUUID().toString();
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
